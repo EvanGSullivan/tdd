f681fede3cb7fb41f08d406d8cfe218e3b5ea76c
Evan Sullivan <evan@grabcad.com>
2016-11-07 22:24:50 -0500

    Add class for test to test.

--- test/money_test.js
+++ test/money_test.js
@@ -1,9 +1,11 @@
 let assert = require('assert');
 
 describe('money tests', function() {
+    let Dollar = require('../dollar.js');
+
     it ('multiplies an amount (price per share) by a number (number of shares).', function() {
         let five = new Dollar(5);
         five.times(2);
-        assert.equals(five.amount, 10);
+        assert.equal(five.amount, 10);
     });
 });
